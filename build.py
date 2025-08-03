006855af8d31d15658ab67d20b49935a50929c3e
sigrid <138397775+sigridjoubert@users.noreply.github.com>
2025-08-03 16:05:01 -0400

    Local build option

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from builder.builder import Builder
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 import os
+import sys
 
 SITE_NAME = "Sigrid Joubert"
 PORT = 8000
@@ -12,17 +13,19 @@ if __name__ == "__main__":
     builder = Builder(SITE_NAME)
     builder.build_site()
 
-    # Serve the site
-    os.chdir(OUTPUT_DIR)  # Change working dir to site output
+    # Check for "local" argument
+    if "local" in sys.argv:
+        os.chdir(OUTPUT_DIR)  # Change working dir to site output
 
-    handler = SimpleHTTPRequestHandler
-    httpd = TCPServer(("", PORT), handler)
+        handler = SimpleHTTPRequestHandler
+        httpd = TCPServer(("", PORT), handler)
 
-    print(f"Serving '{SITE_NAME}' at http://localhost:{PORT}")
-    print("Press Ctrl+C to stop the server.")
+        print(f"Serving '{SITE_NAME}' at http://localhost:{PORT}")
+        print("Press Ctrl+C to stop the server.")
 
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nShutting down the server.")
-        httpd.server_close()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down the server.")
+            httpd.server_close()
+
